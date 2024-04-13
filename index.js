f748b756c071c1ba3db04afa3d41a4e29b9ecf14
robemike <robemike1135@gmail.com>
2024-04-13 10:05:19 +0300

    edits index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+function addingEventListener(event) {
+    console.log('Button clicked!');
+    const input = document.getElementById('button');
+    function clickAlert() {
+        alert('I was clicked')
+    }
+    
+    input.addEventListener('click', clickAlert)
 }
+
+// const button = document.getElementById('myButton');
+
+// button.addEventListener('click', handleClick);
